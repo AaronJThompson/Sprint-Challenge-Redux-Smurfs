9c37f911c36d9a664fbe178aebd0ade61950b445
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-21 09:22:16 +0100

    initial render. Everything seems to be working

--- smurfs/src/components/smurfs/SmurfList.js
+++ smurfs/src/components/smurfs/SmurfList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchSmurfs } from '../../reducers'
+import { fetchSmurfs } from '../../actions'
 import Smurf from './Smurf';
 
 class SmurfList extends React.Component {
