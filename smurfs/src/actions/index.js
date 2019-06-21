305f1917ce23086791e8c384b26364265374b540
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-21 08:52:36 +0100

    Add action creators

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
@@ -7,6 +9,7 @@ actionTypes.FETCHING_SMURFS = 'FETCHING_SMURFS';
 actionTypes.ADDING_SMURF = 'ADDING_SMURF';
 actionTypes.DELETING_SMURF = 'DELETING_SMURF';
 actionTypes.UPDATING_SMURF = 'UPDATING_SMURF';
+actionTypes.SET_SMURFS = 'SET_SMURFS';
 actionTypes.SUCCESS = 'SUCCESS';
 actionTypes.ERROR = 'ERROR';
 /*
@@ -19,3 +22,58 @@ actionTypes.ERROR = 'ERROR';
    U - updateSmurf
    D - deleteSmurf
 */
+export const createAPIUrl = (...paths) => {
+  const url = 'http://localhost:3333/smurfs'
+  if (paths) {
+    paths.forEach(path => {
+      url = url.concat(`/${path}`)
+    })
+  }
+  return url;
+}
+
+export const setSmurfs = (smurfs) => {
+  return {
+    type: actionTypes.SET_SMURFS,
+    payload: smurfs,
+  };
+}
+
+export const setError = (error) => {
+  return {
+    type: actionTypes.ERROR,
+    payload: error,
+  };
+}
+
+export const requestSuccess = () => {
+  return {
+    type: actionTypes.SUCCESS,
+  };
+}
+
+export const fetchSmurfs = () => dispatch => {
+  dispatch({ type: actionTypes.FETCHING_SMURFS });
+  return axios
+    .get(createAPIUrl())
+    .then(res => {
+      dispatch(setSmurfs(res.data));
+      dispatch(requestSuccess());
+    })
+    .catch(error => {
+      dispatch(setError(error.message));
+    })
+};
+
+export const addSmurf = (smurf) => dispatch => {
+  dispatch({ type: actionTypes.ADDING_SMURF });
+  return axios
+    .post(createAPIUrl())
+    .then(res => {
+      dispatch(setSmurfs(res.data));
+      dispatch(requestSuccess());
+    })
+    .catch(error => {
+      dispatch(setError(error.message));
+    })
+} 
